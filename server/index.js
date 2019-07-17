8802952dbe783ce98c6ab71ad77301f0cb35bbe5
Josef Berthoud <josef.berthoud@gmail.com>
2019-07-17 14:09:13 -0700

    rendering 2 components

--- server/index.js
+++ server/index.js
@@ -10,10 +10,21 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.all('/reviews', (req, res) => {
   console.log("redirecting to reviews");
-  apiProxy.web(req, res, {target: 'http://localhost:3000'});
+  console.log(req.params.id);
+  apiProxy.web(req, res, {target: 'http://localhost:3004'});
 });
 
 app.all('/search', (req, res) => {
   console.log("redirecting to reviews");
-  apiProxy.web(req, res, {target: 'http://localhost:3000'});
+  apiProxy.web(req, res, {target: 'http://localhost:3004'});
+});
+
+// app.all('/descriptions', (req, res) => {
+//   console.log("redirecting to descriptions");
+//   apiProxy.web(req, res, {target: 'http://localhost:3001'})
+// });
+
+app.all('/rooms/dates', (req, res) => {
+  console.log("redirecting to checkout");
+  apiProxy.web(req, res, {target: 'http://localhost:3002'});
 });
